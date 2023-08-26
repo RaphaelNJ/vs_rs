6a368808a89382000423d84fa38d7db843a463b9
GNUSwann <52333330+GNUSwann@users.noreply.github.com>
2023-08-26 17:39:53 +0200

    improved a bit the uniquify_name() helper function

--- egui_node_graph_example/src/utils.rs
+++ egui_node_graph_example/src/utils.rs
@@ -5,26 +5,29 @@ pub trait GetName {
     fn get_name(&self) -> String;
 }
 
-pub fn uniquify_name(input_name: String, vec: &Vec<impl GetName>) -> String {
+fn uniquify_name_closure<F>(input_name: String, mut condition: F) -> String
+where
+    F: FnMut(&str) -> bool,
+{
     let mut times = 0;
     let name = input_name.replace(" ", "_");
+
     loop {
         let x = 'x: {
-            for obj in vec.iter() {
-                if times == 0 {
-                    if obj.get_name() == name {
-                        times += 1;
-                        break 'x true;
-                    }
-                } else {
-                    if format!("{}_{}", name, times) == obj.get_name() {
-                        times += 1;
-                        break 'x true;
-                    }
+            if times == 0 {
+                if condition(&name) {
+                    times += 1;
+                    break 'x true;
+                }
+            } else {
+                if condition(&format!("{}_{}", name, times)) {
+                    times += 1;
+                    break 'x true;
                 }
             }
             false
         };
+
         if !x {
             if times == 0 {
                 return name;
@@ -34,32 +37,17 @@ pub fn uniquify_name(input_name: String, vec: &Vec<impl GetName>) -> String {
     }
 }
 
+pub fn uniquify_name(input_name: String, vec: &Vec<impl GetName>) -> String {
+    uniquify_name_closure(input_name, |name| {
+        vec.iter().any(|obj| obj.get_name() == name)
+    })
+}
 
-pub fn uniquify_name_slot(input_name: String, slot: &slotmap::SlotMap<functions::FunctionId, app::GraphFunction>) -> String {
-    let mut times = 0;
-    let name = input_name.replace(" ", "_");
-    loop {
-        let x = 'x: {
-            for obj in slot.values() {
-                if times == 0 {
-                    if obj.get_name() == name {
-                        times += 1;
-                        break 'x true;
-                    }
-                } else {
-                    if format!("{}_{}", name, times) == obj.get_name() {
-                        times += 1;
-                        break 'x true;
-                    }
-                }
-            }
-            false
-        };
-        if !x {
-            if times == 0 {
-                return name;
-            }
-            return format!("{}_{}", name, times);
-        }
-    }
+pub fn uniquify_name_slot(
+    input_name: String,
+    slot: &slotmap::SlotMap<functions::FunctionId, app::GraphFunction>,
+) -> String {
+    uniquify_name_closure(input_name, |name| {
+        slot.values().any(|obj| obj.get_name() == name)
+    })
 }
