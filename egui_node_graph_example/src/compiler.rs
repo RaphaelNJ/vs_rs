aeda142977fe1b7c7a57154673396fcfcea51ada
GNUSwann <52333330+GNUSwann@users.noreply.github.com>
2023-08-29 10:55:48 +0200

    true fennel

--- egui_node_graph_example/src/compiler.rs
+++ egui_node_graph_example/src/compiler.rs
@@ -119,13 +119,12 @@ fn evaluate_functionn(
 
     let script_line = match next_node.user_data.template {
         app::MyNodeTemplate::Enter => "".to_string(),
-        app::MyNodeTemplate::Print => format!("io.write({})", filtered_inputs[0]),
+        app::MyNodeTemplate::Print => format!("(io.write {})", filtered_inputs[0]),
         app::MyNodeTemplate::Ask => {
             format!(
-                "local {} = io.read({}, (func -> {}))",
-                outputs_cache.get(&next_node.outputs[2].1).unwrap(),
+                "(io.write {}) (local {} (io.read))",
                 filtered_inputs[0],
-                executions.get(1).map_or("default", |x| x),
+                outputs_cache.get(&next_node.outputs[2].1).unwrap(),
             )
         }
         app::MyNodeTemplate::Function(_) => "".to_string(),
@@ -134,116 +133,8 @@ fn evaluate_functionn(
     println!("THE LINE -> {}", script_line);
 
     format!(
-        "{} -- {}",
+        "{} {}",
         script_line,
-        executions.get(0).map_or("default", |x| x)
+        executions.get(0).map_or("", |x| x)
     )
-
-    // for x in graph.iter_connections() {
-    //     if
-    //         node
-    //             .output_ids()
-    //             .find(|y| y == &x.1)
-    //             .is_some()
-    //     {
-    //         // println!("{:?}", graph[x.0].node);
-
-    //         if let MyDataType::Execution = graph[x.0].typ {
-    //             evaluate_functionn(graph, graph[x.0].node, outputs_cache);
-    //         }
-
-    //     }
-    // }
-
-    // match node.user_data.template {
-    //     app::MyNodeTemplate::Enter => {}
-    //     app::MyNodeTemplate::Print => {}
-    //     app::MyNodeTemplate::Ask => {}
-    //     app::MyNodeTemplate::Function(_) => {}
-    // }
-}
-
-fn evaluate_function(graph: &MyGraph, enter_node: NodeId) -> String {
-    //println!("{:?}", graph.nodes.get(enter_node).unwrap());
-
-    let mut script = String::new();
-
-    let mut outputs_cache: HashMap<OutputId, String> = HashMap::new();
-
-    let mut current_node = Some(enter_node);
-    let mut next_node = None;
-
-    loop {
-        for x in graph.iter_connections() {
-            if x.1 == graph.nodes.get(current_node.unwrap()).unwrap().outputs[0].1 {
-                let node = graph.nodes.get(graph[x.0].node).unwrap();
-                next_node = Some(node.id);
-
-                let mut outputs = vec![];
-
-                for y in node.inputs(graph) {
-                    if y.typ == MyDataType::Execution {
-                        outputs.push(None);
-                        continue;
-                    }
-                    if let Some(z) = graph.connection(y.id) {
-                        outputs.push(
-                            outputs_cache
-                                .get(&z)
-                                .clone()
-                                .map_or(None, |x| Some(x.to_string()))
-                        );
-                    } else {
-                        outputs.push(
-                            Some(match &y.value {
-                                MyValueType::String { value } => format!("\"{value}\""),
-                                MyValueType::Integer { value } => value.to_string(),
-                                MyValueType::Float { value } => value.to_string(),
-                                MyValueType::Boolean { value } => value.to_string(),
-                                MyValueType::Execution { value } =>
-                                    evaluate_function(graph, current_node.unwrap()),
-                            })
-                        );
-                    }
-                }
-
-                let filtered_outputs: Vec<String> = outputs
-                    .into_iter()
-                    .filter_map(|option| option)
-                    .collect();
-
-                println!("{:?}", filtered_outputs);
-                println!("{:?}", graph.nodes.get(graph[x.0].node).unwrap().user_data.template);
-
-                for y in node.outputs(graph) {
-                    if y.typ == MyDataType::Execution {
-                        continue;
-                    }
-                    outputs_cache.insert(y.id, format!("var_{:?}", y.id.data()));
-                }
-
-                let script_line = match
-                    graph.nodes.get(graph[x.0].node).unwrap().user_data.template
-                {
-                    app::MyNodeTemplate::Enter => "".to_string(),
-                    app::MyNodeTemplate::Print => format!("io.write({})", filtered_outputs[0]),
-                    app::MyNodeTemplate::Ask =>
-                        format!(
-                            "local {} = io.read({})",
-                            outputs_cache.get(&node.outputs[1].1).unwrap(),
-                            filtered_outputs[0]
-                        ),
-                    app::MyNodeTemplate::Function(_) => "".to_string(),
-                };
-
-                script = format!("{script}\n{script_line}");
-            }
-        }
-        if next_node.is_none() {
-            break;
-        }
-        current_node = std::mem::replace(&mut next_node, None);
-    }
-
-    script
 }
